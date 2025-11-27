38a0e191ea31f0e18e16709db035712ca5a9cc7a
CorrosionZoe <corrosionzoemaruoyi@gmail.com>
2025-11-27 15:04:59 -0500

    Lesson 2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <allegro5/allegro.h>
+#include <stdio.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_native_dialog.h>
@@ -6,7 +7,7 @@
 int main(int argc, char *argv[]){
     al_init();
     al_install_keyboard();
-    ALLEGRO_DISPLAY* disp = al_create_display(1000, 700);
+    ALLEGRO_DISPLAY* disp = al_create_display(1000, 1000);
     ALLEGRO_FONT* font = al_create_builtin_font();
 
     if(!disp){
@@ -26,10 +27,13 @@ int main(int argc, char *argv[]){
       	al_destroy_display(disp);
      	return -1;
 	 }
-    al_draw_bitmap(image, 0,  0, 0);
+    ALLEGRO_BITMAP *clipimage = al_create_sub_bitmap(image, 50 , 50, 150, 150);
+    al_draw_bitmap(clipimage , 0, 0, 0); //Draw from 0,0 to 50,50 on the screen at (0,0)
+	al_draw_bitmap(clipimage , 100, 0, 100); //Draw from 0,0 to 50,50 on the screen at (0,0)
     al_flip_display();
     al_rest(5);
     al_destroy_display(disp);
     al_destroy_bitmap(image);
+    
     return 0;
 }
